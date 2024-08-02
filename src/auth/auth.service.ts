eb8a2f3843540b1a79a82526998231e22199243a
valdo <osvaldo@lvvdigital.com>
2024-08-02 13:27:59 -0600

    feat: verify token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,123 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+import { envs } from '../../config/envs';
+import { LoginUserDto, RegisterUserDto } from './dtos';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(AuthService.name);
+
+  constructor(private readonly jwtService: JwtService) {
+    super();
+  }
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Prisma connected <MongoDb>');
+  }
+
+  async signJWT(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    try {
+      const { email, name, password } = registerUserDto;
+
+      const user = await this.user.findUnique({
+        where: { email },
+      });
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exists',
+        });
+      }
+
+      const newUser = await this.user.create({
+        data: {
+          email,
+          name,
+          password: bcrypt.hashSync(password, 10),
+        },
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: _, ...userWithoutPassword } = newUser;
+
+      return {
+        user: userWithoutPassword,
+        token: await this.signJWT(userWithoutPassword),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    try {
+      const { email, password } = loginUserDto;
+
+      const user = await this.user.findUnique({
+        where: { email },
+      });
+
+      if (!user) {
+        throw new RpcException({
+          status: 400,
+          message: 'Credentials are invalid',
+        });
+      }
+
+      const isValidPassword = bcrypt.compareSync(password, user.password);
+
+      if (!isValidPassword) {
+        throw new RpcException({
+          status: 400,
+          message: 'Credentials are invalid',
+        });
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: _, ...userWithoutPassword } = user;
+
+      return {
+        user: userWithoutPassword,
+        token: await this.signJWT(userWithoutPassword),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async verifyToken(token: string) {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { sub, iat, exp, ...user } = this.jwtService.verify(token, {
+        secret: envs.jwtSecret,
+      });
+
+      return {
+        user,
+        token: await this.signJWT(user),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 401,
+        message: 'Token is invalid',
+      });
+    }
+  }
+}
